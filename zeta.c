0a76aebc39a7940d451103a8ac8626ab64b041fc
cercil0605 <kuamaichi2223@gmail.com>
2020-12-25 10:47:39 +0900

    edit

---
+++ zeta.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+
+double zeta(double s){
+
+    int n=1;
+    double ans,a,b;
+    a=0;
+
+    while(1){
+
+        if(n>=2000){
+            break;
+        }
+        b=pow((double)n,s);
+        a=(1.0000/b)+a;
+        printf("%lf  %lf\n",b,a);
+        n++;
+    }
+    return a;
+}
+int main(){
+    double a,b;
+    a=3;
+    b=zeta(a);
+
+    printf("%lf",b);
+}
